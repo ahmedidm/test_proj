adc87a3bd27b79fffe972ef9e48636037ac859ee
AHMED <ahmed@gmail.com>
2022-12-28 22:19:53 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbrComponent } from './navbr/navbr.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbrComponent
+  
   ],
   imports: [
     BrowserModule,
